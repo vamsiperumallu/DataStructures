70b95815671100176dd648b317110f8f054a66fd
Vamsi Perumallu Yalamanchili <vamsiperumallu@gmail.com>
2021-11-24 23:12:07 -0600

    Update BinaryTree.py

--- Searching/BinaryTree.py
+++ Searching/BinaryTree.py
@@ -76,14 +76,60 @@ class BinarySearchTree:
         if curr_node != None:
             self._printTree(curr_node.leftChild)
             print(curr_node.value)
-            self._printTree(curr_node.rightChild)           
+            self._printTree(curr_node.rightChild)
+    
+    def getTreeHeight(self):
+        if self.root != None:
+            return self._getTreeHeight(self.root,0)
+        else:
+            return 0
 
+    #Private function, Can be called from this class only. This is also a recursive function.
+    def _getTreeHeight(self,curr_node,curr_height):
+        
+        if curr_node != None:
+            curr_height = curr_height + 1
+            leftChildLevel  = self._getTreeHeight(curr_node.leftChild, curr_height)        
+            rightChildLevel = self._getTreeHeight(curr_node.rightChild, curr_height)              
+            return max(leftChildLevel, rightChildLevel)
+        else:
+            return curr_height
+    
+    def searchElement(self, element):
+        if self.root != None:
+            return self._searchElement(self.root,element)
+        else:
+            return False
+
+    #Private function, Can be called from this class only. This is also a recursive function.
+    def _searchElement(self,curr_node,element):
+        
+        if element == curr_node.value: return True
+        elif element < curr_node.value and curr_node.leftChild != None:
+            return self._searchElement(curr_node.leftChild,element)
+        elif element > curr_node.value and curr_node.rightChild != None:
+            return self._searchElement(curr_node.rightChild,element)
+        return False
+            
+#    def _searchElement(self,curr_node,element):
+#        
+#        treeHeight = self.getTreeHeight()
+#        for i in range(0,treeHeight):
+#            if curr_node != None:             
+#                if element == curr_node.value:
+#                    return (True,i)
+#                elif element < curr_node.value:                  
+#                    curr_node = curr_node.leftChild                   
+#                else:                      
+#                    curr_node = curr_node.rightChild
+#        return (False,treeHeight)
+        
 def fillBinaryTree(tree):
        
-    seed(111)
+    seed(123)
     
     #Generate 10 random numbers
-    inputList = random.sample(range(1, 100), 10)
+    inputList = random.sample(range(1, 100), 20)
     
     print("Input data", inputList)
     for i in inputList:
@@ -93,5 +139,14 @@ if __name__ == "__main__":
         
     tree = BinarySearchTree()
     fillBinaryTree(tree)
-    tree.printTree()    
-        
+    tree.printTree()
+    print("Height of the tree: ",tree.getTreeHeight())
+    
+    #status, o_n = tree.searchElement(90)
+    status = tree.searchElement(33)
+    if(status):
+        #print("Element FOUND in the tree after searching Nodes:", o_n)
+        print("Element FOUND in the tree")
+    else:
+        #print("Element NOT FOUND in the tree after searching Nodes:", o_n)
+        print("Element NOT FOUND in the tree")
